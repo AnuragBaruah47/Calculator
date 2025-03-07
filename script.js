e1cd44b68a2870ecf32ceee4465b4e55b157c0dc
AnuragBaruah47 <anuragbaruah2004@gmail.com>
2025-03-07 16:28:36 +0530

    CSS Done Js remains

---
+++ script.js
@@ -0,0 +1,5 @@
+const squareButton=document.querySelector(".square")
+const body=document.querySelector("body")
+themeButton.addEventListener("click",function(){
+    body.style.backgroundColor="green"
+})
