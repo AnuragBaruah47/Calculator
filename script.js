c6063d6ee5f92a2207f1331142a6703f3d5788bd
AnuragBaruah47 <anuragbaruah2004@gmail.com>
2025-03-08 17:13:18 +0530

    Sqaure Functionality Added

--- script.js
+++ script.js
@@ -8,10 +8,11 @@ const clear = document.querySelector(".clear");
 const point = document.querySelector(".point");
 const cross = document.querySelector(".cross");
 const number = document.querySelectorAll(".number");
+
 allButtons.forEach((value) => {
-  value.addEventListener("mouseover", () => {
+  value.addEventListener("click", () => {
     let currentname = value.name;
-    current_status.innerText = `${currentname}`;
+    current_status.textContent = `${currentname}`;
   });
 });
 setInterval(() => {
@@ -20,20 +21,40 @@ setInterval(() => {
   clockcontainer.innerText = `${realClock}`;
 }, 1000);
 
-allButtons.forEach((value)=>{
-    value.addEventListener("click",function(){
-        screen.innerText +=`${value.value}`
-    })
-})
-allButtons.forEach((button)=>{
-    button.addEventListener("click",function(){
-            button.style.backgroundColor="#7fff00"
-        setTimeout(()=>{
-            button.style.backgroundColor="#FFFFFF"
-        },100)
-    })
-})
-clear.addEventListener("click",function(){
-    screen.innerText=""
-})
+allButtons.forEach((value) => {
+  value.addEventListener("click", function () {
+    screen.textContent += `${value.value}`;
+    let currentname = value.name;
+    console.log(currentname);
+    let valueofscreen=screen.textContent
+    let int_valueofscreen=parseInt(valueofscreen)
+    let sq_value=Math.pow(int_valueofscreen, 2)
+    if(currentname === "Square"){
+      if (valueofscreen=="") {
+        screen.innerHTML = `<h6>Please Enter A Number First<h6>`
+        squareButton.disabled=true
+        setTimeout(() => {
+          screen.innerHTML = ``
+          squareButton.disabled=false
+        }, 800);
+      }else{
+        screen.textContent = `${sq_value}`;
+        console.log(typeof sq_value ,"here"); 
+        console.log(sq_value);
+        }
+    }
+  });
+});
 
+allButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    button.style.backgroundColor = "#7fff00";
+    setTimeout(() => {
+      button.style.backgroundColor = "#FFFFFF";
+    }, 100);
+  });
+});
+
+clear.addEventListener("click", function () {
+  screen.textContent = "";
+});
